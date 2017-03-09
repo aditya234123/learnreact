6b9a52af39e77a652c8a07416df0dcd7e45e3487
Aditya Yadav <aditya234123@gmail.com>
2017-03-09 12:59:58 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class AppHeader extends Component {
+  render() {
+    return (
+      <div id = 'greeting'>
+        Greeting
+      </div>
+    );
+  }
+}
+
+
+export default AppHeader;
